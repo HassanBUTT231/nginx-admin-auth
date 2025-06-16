8afc51cc039c90f5ca67b70af9361a8d9e5d5067
HassanBUTT231 <156105521+HassanBUTT231@users.noreply.github.com>
2025-06-16 15:42:14 +0500

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get('/', (req, res) => {
 });
 
 const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`App listening on port ${port}`);
 });
